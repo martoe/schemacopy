d605b1ca69c91bebdd9b65046481748547d6e9c6
Martin Ehrnhoefer <martin@bxm.at>
2012-01-10 13:46:10 +0100

    using both system properties and properties-file

--- src/main/java/at/bxm/dbtools/schemacopy/run/SchemaCopyRunner.java
+++ src/main/java/at/bxm/dbtools/schemacopy/run/SchemaCopyRunner.java
@@ -1,5 +1,11 @@
 package at.bxm.dbtools.schemacopy.run;
 
+import at.bxm.dbtools.schemacopy.Database;
+import at.bxm.dbtools.schemacopy.Dialect;
+import at.bxm.dbtools.schemacopy.SchemaCopyException;
+import at.bxm.dbtools.schemacopy.sequence.SequenceAdjuster;
+import at.bxm.dbtools.schemacopy.table.CopyTargetMode;
+import at.bxm.dbtools.schemacopy.table.TableCopier;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,18 +16,10 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.net.URL;
 import java.util.Properties;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import at.bxm.dbtools.schemacopy.Database;
-import at.bxm.dbtools.schemacopy.Dialect;
-import at.bxm.dbtools.schemacopy.SchemaCopyException;
-import at.bxm.dbtools.schemacopy.sequence.SequenceAdjuster;
-import at.bxm.dbtools.schemacopy.table.CopyTargetMode;
-import at.bxm.dbtools.schemacopy.table.TableCopier;
 
 public class SchemaCopyRunner {
 
@@ -93,39 +91,36 @@ public class SchemaCopyRunner {
 	}
 
 	public static void main(String[] args) throws IOException {
+		Properties p = System.getProperties();
 		String propertiesfile = args != null && args.length > 0 ? args[0] : null;
-		Reader is = null;
-		try {
-			Properties p;
-			if (propertiesfile != null) {
-				p = new Properties();
+		if (propertiesfile != null) {
+			Reader is = null;
+			try {
 				is = open(propertiesfile);
 				p.load(is);
-			} else {
-				p = System.getProperties();
-			}
-			SchemaCopyRunner scr = new SchemaCopyRunner();
-			scr.setSource(new Database(
-				new DriverManagerDataSource(p.getProperty("schemacopy.source.url"),
-					p.getProperty("schemacopy.source.username"), p.getProperty("schemacopy.source.password")),
-				Dialect.valueOf(p.getProperty("schemacopy.source.dialect").toUpperCase()),
-				p.getProperty("schemacopy.source.schemaname")));
-			scr.setTarget(new Database(
-				new DriverManagerDataSource(p.getProperty("schemacopy.target.url"),
-					p.getProperty("schemacopy.target.username"), p.getProperty("schemacopy.target.password")),
-				Dialect.valueOf(p.getProperty("schemacopy.target.dialect").toUpperCase()),
-				p.getProperty("schemacopy.target.schemaname")));
-			scr.setCsvDataResource(p.getProperty("schemacopy.datafile"));
-			String batchsize = StringUtils.trimToNull(p.getProperty("schemacopy.batchsize"));
-			if (batchsize != null) {
-				scr.setBatchsize(Integer.parseInt(batchsize));
-			}
-			scr.copy(CopyTargetMode.valueOf(p.getProperty("schemacopy.copymode").toUpperCase()));
-		} finally {
-			if (is != null) {
-				is.close();
+			} finally {
+				if (is != null) {
+					is.close();
+				}
 			}
 		}
+		SchemaCopyRunner scr = new SchemaCopyRunner();
+		scr.setSource(new Database(
+			new DriverManagerDataSource(p.getProperty("schemacopy.source.url"),
+				p.getProperty("schemacopy.source.username"), p.getProperty("schemacopy.source.password")),
+			Dialect.valueOf(p.getProperty("schemacopy.source.dialect").toUpperCase()),
+			p.getProperty("schemacopy.source.schemaname")));
+		scr.setTarget(new Database(
+			new DriverManagerDataSource(p.getProperty("schemacopy.target.url"),
+				p.getProperty("schemacopy.target.username"), p.getProperty("schemacopy.target.password")),
+			Dialect.valueOf(p.getProperty("schemacopy.target.dialect").toUpperCase()),
+			p.getProperty("schemacopy.target.schemaname")));
+		scr.setCsvDataResource(p.getProperty("schemacopy.datafile"));
+		String batchsize = StringUtils.trimToNull(p.getProperty("schemacopy.batchsize"));
+		if (batchsize != null) {
+			scr.setBatchsize(Integer.parseInt(batchsize));
+		}
+		scr.copy(CopyTargetMode.valueOf(p.getProperty("schemacopy.copymode").toUpperCase()));
 	}
 
 	private Reader getCsvDataReader() throws IOException {
